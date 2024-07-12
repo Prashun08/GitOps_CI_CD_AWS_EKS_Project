1e6e1616b76ef33c11267ec475cf77abd43dbd3d
Prashun Chakraborty <prashun.ch@gmail.com>
2024-07-12 14:18:55 +0530

    Modified CI

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -3,9 +3,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.27"
     }
   }
+  backend "http" {}
 }
 provider "aws" {
   region = "us-east-1"
